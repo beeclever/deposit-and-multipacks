a9f5e0802a48ccecfa6f1fbfa250db7fdb5cd213
André Neder <andre@beeclever.de>
2024-09-01 15:08:57 +0200

    changes

--- extensions/product-configuration/src/ProductVariantDetailsConfigurationExtension.tsx
+++ extensions/product-configuration/src/ProductVariantDetailsConfigurationExtension.tsx
@@ -12,42 +12,44 @@ import { useQuery } from "./hooks/useQuery";
 
 export default reactExtension<any>('admin.product-variant-details.configuration.render', () => <App />);
 
-function App() {
+function App(){
   
   const {extension: {target}, data: {variant}, i18n} = useApi<'admin.product-variant-details.configuration.render'>();
 
 
-  const { data: { productVariant }, loading, errors } = useQuery(`
-  query productVariant($id: ID!){
-    productVariant(id: $id) {
-      deposit: metafield(namespace: "$app", key: "deposit"){
-        value
-      }
-      quantity: metafield(namespace: "$app", key: "quantity"){
-        value
-      }
-      baseVariant: metafield(namespace: "$app", key: "base_variant"){
-        value
-      }
-    }
-  }`, {
-    id: `gid://shopify/ProductVariant/${variant.id}`
-  })
+  // const { data: { productVariant }, loading, errors } = useQuery(`
+  // query productVariant($id: ID!){
+  //   productVariant(id: $id) {
+  //     deposit: metafield(namespace: "$app", key: "deposit"){
+  //       value
+  //     }
+  //     quantity: metafield(namespace: "$app", key: "quantity"){
+  //       value
+  //     }
+  //     baseVariant: metafield(namespace: "$app", key: "base_variant"){
+  //       value
+  //     }
+  //   }
+  // }`, {
+  //   id: `gid://shopify/ProductVariant/${variant.id}`
+  // })
 
-  console.log(productVariant)
-  // return <></>
-  return loading 
-    ? <>
-      {/* <ProgressIndicator size="small-200" /> */}
-    </> 
-    : <>
-    	<InlineStack gap>
-        {/* <Image alt="Pickaxe" source="/assets/icons/64/pickaxe-1.png" /> */}
-        <Text>
-          {productVariant?.baseVariant?.value ?? ""}
-        </Text>
-        <NumberField label="Quantity" readOnly value={productVariant?.quantity?.value ?? ""} />
-      </InlineStack>
-     ^
-    </>
+  // console.log(productVariant)
+  return <Text>
+      {i18n.translate('variantConfiguration')}
+   </Text>
+  // return loading 
+  //   ? <>
+  //     {/* <ProgressIndicator size="small-200" /> */}
+  //   </> 
+  //   : <>
+  //   	<InlineStack gap>
+  //       {/* <Image alt="Pickaxe" source="/assets/icons/64/pickaxe-1.png" /> */}
+  //       <Text>
+  //         {productVariant?.baseVariant?.value ?? ""}
+  //       </Text>
+  //       <NumberField label="Quantity" readOnly value={productVariant?.quantity?.value ?? ""} />
+  //     </InlineStack>
+     
+  //   </>
 }
